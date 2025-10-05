081922a3a9fced14030f2054d73dc49bdb748878
Rasel-Shikder <rasel.sikder777.rk@gmail.com>
2025-10-05 23:21:56 +0600

    fixed cors for all frontend url

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 import express, { type Application, type Request, type Response } from "express"
 import cors from "cors"
 import compression from "compression"
-import { envVars } from "./configs/envVars.js"
+// import { envVars } from "./configs/envVars.js"
 import notFound from "./middlewares/notFound.js"
 import globalError from "./middlewares/globalError.js"
 import { appRoutes } from "./routes/mainRouter.js"
@@ -15,10 +15,19 @@ app.use(compression())
 app.use(express.json())
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser())
+// app.use(cors({
+//     origin:envVars.FRONTEND_URL as string,
+//     credentials:true
+// }))
 app.use(cors({
-    origin:envVars.FRONTEND_URL as string,
-    credentials:true
-}))
+  origin: (origin, callback) => {
+    // Allow requests with no origin (like Postman, curl)
+    if (!origin) return callback(null, true);
+    // Otherwise, allow the frontend that made the request
+    callback(null, true);
+  },
+  credentials: true, // Allow cookies and auth headers
+}));
 
 app.use("/api/v1", appRoutes)
 
