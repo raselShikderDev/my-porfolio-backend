c4959297fc5854788f01ceaa2ae69be7344e7ba0
Rasel-Shikder <rasel.sikder777.rk@gmail.com>
2025-10-09 16:18:20 +0600

    refactor request validator

--- src/middlewares/requestValidator.ts
+++ src/middlewares/requestValidator.ts
@@ -6,33 +6,17 @@ import { envVars } from '../configs/envVars';
 export const requestValidator =
   (zodSchema: ZodObject<ZodRawShape>) =>
   async (req: Request, res: Response, next: NextFunction) => {
-    try {
-      if (envVars.NODE_ENV === 'development') {
-        console.log(
-          '[Very first] Validating data of received request',
-          `${req.body.data ? req.body.data : req.body}`,
-        );
-      }
+    if (envVars.NODE_ENV === 'Development')
+      console.log(`in validateReq - req.body: `, req.body);
 
-      if (req.body.data) {
-        // req.body = JSON.parse(req.body.data);
-        req.body = req.body.data;
-        console.log(
-          '[req.body.data] Data from received request is validated',
-          req.body.data,
-        );
-      }
-      await zodSchema.parseAsync(req.body);
-      if (envVars.NODE_ENV === 'development') {
-        console.log(
-          '[req.body] Data from received request is validated',
-          req.body,
-        );
-      }
-      next();
-    } catch (error) {
-      if (envVars.NODE_ENV === 'development')
-        console.log('errorin validator: ', error);
-      next(error);
+    if (req.body.data) {
+      if (envVars.NODE_ENV === 'Development')
+        console.log(`in validateReq - req.body.data: `, req.body.data);
+      req.body = JSON.parse(req.body.data);
     }
+    req.body = await zodSchema.parseAsync(req.body);
+    if (envVars.NODE_ENV === 'Development')
+      console.log(`in validateReq after validation - payload: `, req.body);
+
+    next();
   };
