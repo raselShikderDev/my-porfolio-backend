7844ca1123dd07c20db1ba4ee6a19ab246fac87f
Rasel SHikder <rasel.sikder777.rk@gmail.com>
2026-09-25 21:42:43 +0600

    refactor: validate environment configuration

--- src/configs/envVars.ts
+++ src/configs/envVars.ts
@@ -9,7 +9,7 @@ interface IEnvVars {
   FRONTEND_URL: string;
   OWNER_EMAIL: string;
   OWNER_PASSWORD: string;
-  BCRYPT_SALT: string;
+  BCRYPT_SALT: number;
   JWT_ACCESS_SECRET: string;
   JWT_ACCESS_EXPIRES: string;
   JWT_REFRESH_SECRET: string;
@@ -20,8 +20,8 @@ interface IEnvVars {
   CLOUDINARY_URL: string;
 }
 
-const envvarriables = (): IEnvVars => {
-  const varriables: string[] = [
+const envVariables = (): IEnvVars => {
+  const variables: string[] = [
     "PORT",
     "NODE_ENV",
     "DATABASE_URL",
@@ -39,9 +39,9 @@ const envvarriables = (): IEnvVars => {
     "CLOUDINARY_URL",
   ];
 
-  varriables.map((envItem: string) => {
+  variables.forEach((envItem: string) => {
     if (!process.env[envItem]) {
-      throw new Error(`Missing envoirnment varriabls ${envItem}`);
+      throw new Error(`Missing environment variable: ${envItem}`);
     }
   });
 
@@ -52,7 +52,7 @@ const envvarriables = (): IEnvVars => {
     FRONTEND_URL: process.env.FRONTEND_URL as string,
     OWNER_EMAIL: process.env.OWNER_EMAIL as string,
     OWNER_PASSWORD: process.env.OWNER_PASSWORD as string,
-    BCRYPT_SALT: process.env.BCRYPT_SALT as string,
+    BCRYPT_SALT: parseInt(process.env.BCRYPT_SALT as string),
     JWT_REFRESH_EXPIRES: process.env.JWT_REFRESH_EXPIRES as string,
     JWT_REFRESH_SECRET: process.env.JWT_REFRESH_SECRET as string,
     JWT_ACCESS_EXPIRES: process.env.JWT_ACCESS_EXPIRES as string,
@@ -64,4 +64,4 @@ const envvarriables = (): IEnvVars => {
   };
 };
 
-export const envVars = envvarriables();
+export const envVars = envVariables();
